1b3ef4c30cf30e44570952f96bb535e0c0d072dc
qarretado <qarretadopb@gmail.com>
2025-01-15 23:12:36 -0300

    Initial commit

---
+++ GoogleTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+public class SeleniumTest
+{
+    [Fact]
+    public void AbrirGoogle_DeveExibirPaginaInicial()
+    {
+
+        new DriverManager().SetUpDriver(new ChromeConfig());
+
+        using (var driver = new ChromeDriver())
+        {
+            driver.Navigate().GoToUrl("https://www.google.com.br");
+
+            Assert.Equal("Google", driver.Title);
+        }
+    }
+}
